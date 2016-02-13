e19941ca163f1e11e83838238b5454c87c3732d4
Hamish Knight <hk@southerncare.co.uk>
2016-02-13 19:00:23 +0000

    added some extra colors

--- Line View/ViewController.swift
+++ Line View/ViewController.swift
@@ -14,15 +14,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let lineView = LineView()
-        lineView.frame = CGRectMake(50, 50, 20, view.bounds.size.height-100)
+        let lineView = LineView(frame: CGRectMake(50, 50, 20, view.bounds.size.height-100))
+        
+        lineView.colors = [
+            UIColor(red: 1.0, green: 31.0/255.0, blue: 73.0/255.0, alpha: 1.0), // red
+            UIColor(red:1.0, green: 138.0/255.0, blue: 0.0, alpha:1.0), // orange
+            UIColor(red: 122.0/255.0, green: 108.0/255.0, blue: 1.0, alpha: 1.0), // purple
+            UIColor(red: 0.0, green: 100.0/255.0, blue: 1.0, alpha: 1.0), // dark blue
+            UIColor(red: 100.0/255.0, green: 241.0/255.0, blue: 183.0/255.0, alpha: 1.0), // green
+            UIColor(red: 0.0, green: 222.0/255.0, blue: 1.0, alpha: 1.0) // blue
+        ]
+        lineView.values = [0.15, 0.1, 0.35, 0.15, 0.1, 0.15]
+        
         view.addSubview(lineView);
-        
-        
-        lineView.colors = [UIColor(red: 122.0/255.0, green: 108.0/255.0, blue: 1.0, alpha: 1.0),
-                           UIColor(red: 100.0/255.0, green: 241.0/255.0, blue: 183.0/255.0, alpha: 1.0),
-                           UIColor(red: 0.0, green: 222.0/255.0, blue: 1.0, alpha: 1.0)]
-        lineView.values = [0.35, 0.45, 0.2]
+
         
     }
     
